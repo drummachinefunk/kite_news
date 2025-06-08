db4c82b12811a94a9ce3a091a0d6a5f09b779824
rmri <intonarumori@gmail.com>
2025-06-08 13:23:47 +0200

    cleanup

--- lib/features/story_details/story_details_page.dart
+++ lib/features/story_details/story_details_page.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:kagi_news/features/story_details/components/article_box.dart';
-import 'package:kagi_news/features/story_details/components/bullet_list.dart';
-import 'package:kagi_news/features/story_details/components/carousel_list.dart';
-import 'package:kagi_news/features/story_details/components/numbered_list.dart';
-import 'package:kagi_news/features/story_details/story_details_bloc.dart';
-import 'package:kagi_news/features/story_details/components/quote_box.dart';
-import 'package:kagi_news/features/story_details/components/sliver_with_padding.dart';
-import 'package:kagi_news/features/story_details/components/sliver_spacing.dart';
-import 'package:kagi_news/features/story_details/utilities/numbered_list_helpers.dart';
+
+import 'components/article_box.dart';
+import 'components/bullet_list.dart';
+import 'components/carousel_list.dart';
+import 'components/numbered_list.dart';
+import 'story_details_bloc.dart';
+import 'components/quote_box.dart';
+import 'components/sliver_with_padding.dart';
+import 'components/sliver_spacing.dart';
+import 'utilities/numbered_list_helpers.dart';
 
 class StoryDetailsPage extends StatefulWidget {
   const StoryDetailsPage({super.key});
@@ -25,148 +26,143 @@ class _StoryDetailsPageState extends State<StoryDetailsPage> {
         final article1 = state.cluster.articles.isNotEmpty ? state.cluster.articles.first : null;
         final article2 = state.cluster.articles.length > 1 ? state.cluster.articles[1] : null;
 
-        return Scaffold(
-          body: CustomScrollView(
-            physics: const ClampingScrollPhysics(),
-            slivers: [
-              SliverWithPadding(
-                child: Text(state.cluster.category, style: Theme.of(context).textTheme.titleSmall),
+        return CustomScrollView(
+          physics: const ClampingScrollPhysics(),
+          slivers: [
+            SliverWithPadding(
+              child: Text(state.cluster.category, style: Theme.of(context).textTheme.titleSmall),
+            ),
+            SliverSpacing(height: 3),
+            SliverWithPadding(
+              child: Text(state.cluster.title, style: Theme.of(context).textTheme.displaySmall),
+            ),
+            SliverSpacing(),
+            SliverWithPadding(
+              child: Text(
+                state.cluster.shortSummary,
+                style: Theme.of(context).textTheme.bodyMedium,
               ),
-              SliverSpacing(height: 3),
-              SliverWithPadding(
-                child: Text(state.cluster.title, style: Theme.of(context).textTheme.displaySmall),
-              ),
-              SliverSpacing(),
-              SliverWithPadding(
-                child: Text(
-                  state.cluster.shortSummary,
-                  style: Theme.of(context).textTheme.bodyMedium,
+            ),
+            SliverSpacing(),
+            if (article1 != null) ...[
+              SliverToBoxAdapter(
+                child: ArticleBox(
+                  imageUrl: article1.image,
+                  caption: article1.imageCaption,
+                  source: article1.domain,
+                  url: article1.link,
                 ),
               ),
-              SliverSpacing(),
-              if (article1 != null) ...[
-                SliverToBoxAdapter(
-                  child: ArticleBox(
-                    imageUrl: article1.image,
-                    caption: article1.imageCaption,
-                    source: article1.domain,
-                    url: article1.link,
-                  ),
-                ),
-                SliverSpacing(),
-              ],
-              if (state.cluster.talkingPoints.isNotEmpty) ...[
-                SliverWithPadding(
-                  child: NumberedList(
-                    title: 'Highlights',
-                    items: NumberedListHelper.itemsFromList(
-                      state.cluster.talkingPoints,
-                      separator: ':',
-                    ),
-                  ),
-                ),
-                SliverSpacing(),
-              ],
-              if (state.cluster.destinationHighlights.isNotEmpty) ...[
-                SliverWithPadding(
-                  child: BulletList(
-                    title: 'Destination Highlights',
-                    items: state.cluster.destinationHighlights,
-                  ),
-                ),
-                SliverSpacing(),
-              ],
-              if (state.cluster.perspectives.isNotEmpty) ...[
-                SliverToBoxAdapter(
-                  child: CarouselList(
-                    title: 'Perspectives',
-                    items:
-                        state.cluster.perspectives
-                            .map(
-                              (e) => CarouselListItemData(
-                                text: e.text,
-                                source: e.sources.first.name,
-                                url: e.sources.first.url,
-                              ),
-                            )
-                            .toList(),
-                  ),
-                ),
-                SliverSpacing(),
-              ],
-              if (state.cluster.timeline.isNotEmpty) ...[
-                SliverWithPadding(
-                  child: NumberedList(
-                    title: 'Timeline',
-                    items: NumberedListHelper.itemsFromList(state.cluster.timeline),
-                  ),
-                ),
-                SliverSpacing(),
-              ],
-              if (state.cluster.destinationHighlights.isNotEmpty) ...[
-                SliverWithPadding(
-                  child: BulletList(
-                    title: 'Destination Highlights',
-                    items: state.cluster.destinationHighlights,
-                  ),
-                ),
-                SliverSpacing(),
-              ],
-              if (state.cluster.gameplayMechanics.isNotEmpty) ...[
-                SliverWithPadding(
-                  child: BulletList(
-                    title: 'Gameplay Mechanics',
-                    items: state.cluster.gameplayMechanics,
-                  ),
-                ),
-                SliverSpacing(),
-              ],
-              if (state.cluster.industryImpact.isNotEmpty) ...[
-                SliverWithPadding(
-                  child: BulletList(title: 'Industry Impact', items: state.cluster.industryImpact),
-                ),
-                SliverSpacing(),
-              ],
-              if (article2 != null) ...[
-                SliverToBoxAdapter(
-                  child: ArticleBox(
-                    imageUrl: article2.image,
-                    caption: article2.imageCaption,
-                    source: article2.domain,
-                    url: article2.link,
-                  ),
-                ),
-                SliverSpacing(),
-              ],
-              if (state.cluster.technicalDetails.isNotEmpty) ...[
-                SliverWithPadding(
-                  child: BulletList(
-                    title: 'Technical Details',
-                    items: state.cluster.technicalDetails,
-                  ),
-                ),
-                SliverSpacing(),
-              ],
-              if (state.cluster.userActionItems.isNotEmpty) ...[
-                SliverWithPadding(
-                  child: BulletList(
-                    title: 'User Action Items',
-                    items: state.cluster.userActionItems,
-                  ),
-                ),
-                SliverSpacing(),
-              ],
-              if (state.cluster.didYouKnow.isNotEmpty) ...[
-                SliverWithPadding(
-                  child: QuoteBox(title: 'Did You Know', text: state.cluster.didYouKnow),
-                ),
-                SliverSpacing(),
-              ],
-              SliverSpacing(),
-              SliverSpacing(),
               SliverSpacing(),
             ],
-          ),
+            if (state.cluster.talkingPoints.isNotEmpty) ...[
+              SliverWithPadding(
+                child: NumberedList(
+                  title: 'Highlights',
+                  items: NumberedListHelper.itemsFromList(
+                    state.cluster.talkingPoints,
+                    separator: ':',
+                  ),
+                ),
+              ),
+              SliverSpacing(),
+            ],
+            if (state.cluster.destinationHighlights.isNotEmpty) ...[
+              SliverWithPadding(
+                child: BulletList(
+                  title: 'Destination Highlights',
+                  items: state.cluster.destinationHighlights,
+                ),
+              ),
+              SliverSpacing(),
+            ],
+            if (state.cluster.perspectives.isNotEmpty) ...[
+              SliverToBoxAdapter(
+                child: CarouselList(
+                  title: 'Perspectives',
+                  items:
+                      state.cluster.perspectives
+                          .map(
+                            (e) => CarouselListItemData(
+                              text: e.text,
+                              source: e.sources.first.name,
+                              url: e.sources.first.url,
+                            ),
+                          )
+                          .toList(),
+                ),
+              ),
+              SliverSpacing(),
+            ],
+            if (state.cluster.timeline.isNotEmpty) ...[
+              SliverWithPadding(
+                child: NumberedList(
+                  title: 'Timeline',
+                  items: NumberedListHelper.itemsFromList(state.cluster.timeline),
+                ),
+              ),
+              SliverSpacing(),
+            ],
+            if (state.cluster.destinationHighlights.isNotEmpty) ...[
+              SliverWithPadding(
+                child: BulletList(
+                  title: 'Destination Highlights',
+                  items: state.cluster.destinationHighlights,
+                ),
+              ),
+              SliverSpacing(),
+            ],
+            if (state.cluster.gameplayMechanics.isNotEmpty) ...[
+              SliverWithPadding(
+                child: BulletList(
+                  title: 'Gameplay Mechanics',
+                  items: state.cluster.gameplayMechanics,
+                ),
+              ),
+              SliverSpacing(),
+            ],
+            if (state.cluster.industryImpact.isNotEmpty) ...[
+              SliverWithPadding(
+                child: BulletList(title: 'Industry Impact', items: state.cluster.industryImpact),
+              ),
+              SliverSpacing(),
+            ],
+            if (article2 != null) ...[
+              SliverToBoxAdapter(
+                child: ArticleBox(
+                  imageUrl: article2.image,
+                  caption: article2.imageCaption,
+                  source: article2.domain,
+                  url: article2.link,
+                ),
+              ),
+              SliverSpacing(),
+            ],
+            if (state.cluster.technicalDetails.isNotEmpty) ...[
+              SliverWithPadding(
+                child: BulletList(
+                  title: 'Technical Details',
+                  items: state.cluster.technicalDetails,
+                ),
+              ),
+              SliverSpacing(),
+            ],
+            if (state.cluster.userActionItems.isNotEmpty) ...[
+              SliverWithPadding(
+                child: BulletList(title: 'User Action Items', items: state.cluster.userActionItems),
+              ),
+              SliverSpacing(),
+            ],
+            if (state.cluster.didYouKnow.isNotEmpty) ...[
+              SliverWithPadding(
+                child: QuoteBox(title: 'Did You Know', text: state.cluster.didYouKnow),
+              ),
+              SliverSpacing(),
+            ],
+            SliverSpacing(),
+            SliverSpacing(),
+            SliverSpacing(),
+          ],
         );
       },
     );
